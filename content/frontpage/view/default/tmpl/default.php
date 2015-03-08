09ddad72484b5752e8c8bfbca4dc1def4509e1f2
makiaho <pekka.makiaho@uta.fi>
2015-03-08 22:58:25 +0200

    Kopioitu Omanelamaniluojan index.html -sivu Frontpage-appiksen tmpl-hakemistoon default.php-tiedostoksi (ja alkuperäinen nimetty default2.php:ksi) että pääsee katsomaan, miltä sivu ALUSTAVASTI näyttää

--- content/frontpage/view/default/tmpl/default.php
+++ content/frontpage/view/default/tmpl/default.php
@@ -1,66 +1,337 @@
-<h1><?php echo SomeText::_('OMANELAMANILUOJA')  ?></h1>
-<p>This is www-programming course framework, named wo2009.</p>
-<pre style="color:red">
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="generator" content="CoffeeCup HTML Editor (www.coffeecup.com)">
+    <meta name="created" content="Thu, 13 Sep 2012 06:55:34 GMT">
+    <meta name="description" content="Oman El&auml;m&auml;ni Luoja - Henkisi&auml; ja k&auml;yt&auml;nn&ouml;llisi&auml; v&auml;lineit&auml; el&auml;m&auml;n tietoiseen luomiseen">
+    <meta name="keywords" content="manifestointi, unelmat, henkisyys, pelko">
+    <title>Oman el&auml;m&auml;ni luoja</title>
+    
 
-Crud application has new COLUMN in the database. ownerid, integer. Create it.
-
-ALTER TABLE `crud` ADD `ownerid` INT NULL ,
-ADD INDEX ( `ownerid` ) ;
-</pre>
-<pre style="color:blue">You should have now, from week 6 onwards the database 
-configured! You need to have configuration.php that has your values.<br >/<br />
-
-Copy your old configuration.php if you have changed it to this new framework folder.
-</pre>
-
-
-<table border="1" class="sitemaptable">
-
-<tr>
-	<td>
+    <!--[if IE]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+	<link rel="stylesheet" type="text/css" href="tyylitiedosto.css">
 	
-	<a href="index.php?app=sitemap">Extension index</a>
-	
-	</td>
-	
-	<td>
-	<p>
-	A Collection of extensions with explanations what they are.
-	</p>
-	
-	</td>
-</tr>
+	<link href="http://cdn-images.mailchimp.com/embedcode/classic-081711.css" rel="stylesheet" type="text/css">
+<style type="text/css">
+	#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
+	/* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+	   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+</style>
+  </head>
+  <body>
+<div id="fb-root" pbzloc="0">&nbsp;</div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/fi_FI/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 
-<tr>
-	<td>
-	
-	<a href="index.php?app=help">Help</a>
-	
-	</td>
-	
-	<td>
-	<p>
-	Some instructions to get working database connection.
-	</p>
-	
-	</td>
-</tr>
+<center>
+<table align="center" border="0" cellpadding="0" cellspacing="0" width="1000">
+	<tbody>
+		<tr>
+			<td class="top" height="248" width="1000">&nbsp;</td>
+		</tr>
+		<tr>
+			<td align="center" class="valikko" height="40" width="1000">
+			<p class="valikko"><a href="index.html">Koti</a> | <a href="manifestointi.html">Manifestointi</a> | <a href="mentorointi.html">Mentorointi</a> | <a href="kurssit.html">Kurssit</a> | <a href="tuotteet.html">Tuotteet</a> | <a href="ajankohtaista.html">Ajankohtaista</a> | <a href="taustaa.html">Taustaa</a> | <a href="http://omanelamaniluoja.fi/blogi">Blogi</a> | <a href="yhteystiedot.html">Yhteystiedot</a> | <a href="http://facebook.com/omanelamaniluoja" target="_blank">Fb</a></p>
+			</td>
+		</tr>
+		<tr>
+			<td align="left" class="sisalto" height="1000" valign="top" width="1000"><br />
+			<h1>Luo el&auml;m&auml;&auml;si tietoisesti, syvimmist&auml; toiveistasi k&auml;sin</h1>
+			<p>
+			
+			
+			</p>
+			<table align="center" border="0" cellpadding="0" cellspacing="0" class="uutiskirje" height="65" width="800">
+				<tbody>
+					<tr>
+						<td height="80" valign="top" width="525">
+						<p class="laatikko">N&auml;iden sivujen tarkoitus on auttaa sinua el&auml;m&auml;&auml;n todeksi t&auml;ysi potentiaalisi. T&auml;ydess&auml; potentiaalissaan oleva ihminen 
+						on aidosti ja avoimesti oma itsens&auml;. H&auml;n ilmaisee itse&auml;&auml;n vapaasti ja aidosti. T&auml;ydess&auml; potentiaalissaan oleva 
+						ihminen tuntee todellisen arvonsa. H&auml;n ei kuvittele olevansa huonompi tai parempi kuin toiset, vaan tiedostaa 
+						oman ainutlaatuisen upeutensa ja tiet&auml;&auml; olevansa yht&auml; arvokas kuin kuka tahansa toinen.<br><br>
+						T&auml;ydess&auml; potentiaalissaan oleva ihminen on rohkea. H&auml;n uskaltaa antautua syv&auml;lt&auml; sis&auml;lt&auml;&auml;n kumpuaville toiveille 
+						ja unelmille, ja antaa el&auml;m&auml;ns&auml; muovautua niiden mukaiseksi. H&auml;n tekee my&ouml;s itse joka p&auml;iv&auml; aktiivisesti kaikkensa 
+						toteuttaakseen syvimm&auml;t unelmansa.<br><br>
+						T&auml;ydess&auml; potentiaalissaan el&auml;miseen kuuluu syv&auml; t&auml;yttymys. Siihen kuuluvat jatkuvasti syvenev&auml; aito ja puhdas ilo 
+						el&auml;m&auml;st&auml;, keveys, vapaus, rakkaus, luottamus ja kiitollisuus.</p>
+						</td>
+						<td height="80" width="275"><!-- Begin MailChimp Signup Form -->
+						<div id="mc_embed_signup">
+						<form action="http://hiljaisuudenpuutarha.us5.list-manage.com/subscribe/post?u=cbfbabcedcb7cc81bbba3eca3&amp;id=e235d172b9" class="validate" id="mc-embedded-subscribe-form" method="post" name="mc-embedded-subscribe-form" novalidate="" target="_blank">
+						<p class="laatikko3">Rohkeutta oman tien kulkemiseen - Tilatessasi uutiskirjeen saat ilmaisen manifestointi&auml;&auml;nitteen</p>
 
-<tr>
-	<td>
-	
-	<a href="wodoc">API Docs and examples</a>
-	
-	</td>
-	
-	<td>
-	<p>
-	Autogenerated API documentation with few examples.
-	</p>
+						<div class="mc-field-group"><label for="mce-EMAIL">S&auml;hk&ouml;postiosoite </label><input class="required email" id="mce-EMAIL" name="EMAIL" type="text" /></div>
 
-	</td>
-</tr>
+						<div class="clear" id="mce-responses">
+						<div class="response" id="mce-error-response" style="display: none">&nbsp;</div>
 
+						<div class="response" id="mce-success-response" style="display: none">&nbsp;</div>
+						</div>
 
+						<div class="clear"><input class="button" id="mc-embedded-subscribe" name="subscribe" type="submit" value="Tilaa uutiskirje" /></div>
+						</form>
+						</div>
+						<script type="text/javascript">
+var fnames = new Array();var ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';
+try {
+    var jqueryLoaded=jQuery;
+    jqueryLoaded=true;
+} catch(err) {
+    var jqueryLoaded=false;
+}
+var head= document.getElementsByTagName('head')[0];
+if (!jqueryLoaded) {
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js';
+    head.appendChild(script);
+    if (script.readyState && script.onload!==null){
+        script.onreadystatechange= function () {
+              if (this.readyState == 'complete') mce_preload_check();
+        }    
+    }
+}
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = 'http://downloads.mailchimp.com/js/jquery.form-n-validate.js';
+head.appendChild(script);
+var err_style = '';
+try{
+    err_style = mc_custom_error_style;
+} catch(e){
+    err_style = '#mc_embed_signup input.mce_inline_error{border-color:#6B0505;} #mc_embed_signup div.mce_inline_error{margin: 0 0 1em 0; padding: 5px 10px; background-color:#6B0505; font-weight: bold; z-index: 1; color:#fff;}';
+}
+var head= document.getElementsByTagName('head')[0];
+var style= document.createElement('style');
+style.type= 'text/css';
+if (style.styleSheet) {
+  style.styleSheet.cssText = err_style;
+} else {
+  style.appendChild(document.createTextNode(err_style));
+}
+head.appendChild(style);
+setTimeout('mce_preload_check();', 250);
 
+var mce_preload_checks = 0;
+function mce_preload_check(){
+    if (mce_preload_checks>40) return;
+    mce_preload_checks++;
+    try {
+        var jqueryLoaded=jQuery;
+    } catch(err) {
+        setTimeout('mce_preload_check();', 250);
+        return;
+    }
+    try {
+        var validatorLoaded=jQuery("#fake-form").validate({});
+    } catch(err) {
+        setTimeout('mce_preload_check();', 250);
+        return;
+    }
+    mce_init_form();
+}
+function mce_init_form(){
+    jQuery(document).ready( function($) {
+      var options = { errorClass: 'mce_inline_error', errorElement: 'div', onkeyup: function(){}, onfocusout:function(){}, onblur:function(){}  };
+      var mce_validator = $("#mc-embedded-subscribe-form").validate(options);
+      $("#mc-embedded-subscribe-form").unbind('submit');//remove the validator so we can get into beforeSubmit on the ajaxform, which then calls the validator
+      options = { url: 'http://hiljaisuudenpuutarha.us5.list-manage2.com/subscribe/post-json?u=cbfbabcedcb7cc81bbba3eca3&id=e235d172b9&c=?', type: 'GET', dataType: 'json', contentType: "application/json; charset=utf-8",
+                    beforeSubmit: function(){
+                        $('#mce_tmp_error_msg').remove();
+                        $('.datefield','#mc_embed_signup').each(
+                            function(){
+                                var txt = 'filled';
+                                var fields = new Array();
+                                var i = 0;
+                                $(':text', this).each(
+                                    function(){
+                                        fields[i] = this;
+                                        i++;
+                                    });
+                                $(':hidden', this).each(
+                                    function(){
+                                        var bday = false;
+                                        if (fields.length == 2){
+                                            bday = true;
+                                            fields[2] = {'value':1970};//trick birthdays into having years
+                                        }
+                                    	if ( fields[0].value=='MM' && fields[1].value=='DD' && (fields[2].value=='YYYY' || (bday && fields[2].value==1970) ) ){
+                                    		this.value = '';
+									    } else if ( fields[0].value=='' && fields[1].value=='' && (fields[2].value=='' || (bday && fields[2].value==1970) ) ){
+                                    		this.value = '';
+									    } else {
+									        if (/\[day\]/.test(fields[0].name)){
+    	                                        this.value = fields[1].value+'/'+fields[0].value+'/'+fields[2].value;									        
+									        } else {
+    	                                        this.value = fields[0].value+'/'+fields[1].value+'/'+fields[2].value;
+	                                        }
+	                                    }
+                                    });
+                            });
+                        return mce_validator.form();
+                    }, 
+                    success: mce_success_cb
+                };
+      $('#mc-embedded-subscribe-form').ajaxForm(options);
+      /*
+ * Translated default messages for the jQuery validation plugin.
+ * Locale: FI
+ */
+jQuery.extend(jQuery.validator.messages, {
+	required: "T&auml;m&auml; kentt&auml; on pakollinen.",
+	maxlength: jQuery.validator.format("Voit sy&ouml;tt&auml;&auml; enint&auml;&auml;n {0} merkki&auml;."),
+	minlength: jQuery.validator.format("V&auml;hint&auml;&auml;n {0} merkki&auml;."),
+	rangelength: jQuery.validator.format("Sy&ouml;t&auml; v&auml;hint&auml;&auml;n {0} ja enint&auml;&auml;n {1} merkki&auml;."),
+	email: "Sy&ouml;t&auml; oikea s&auml;hk&ouml;postiosoite.",
+	url: "Sy&ouml;t&auml; oikea URL osoite.",
+	date: "Sy&ouml;t&auml; oike p&auml;iv&auml;m&auml;&auml;r&auml;.",
+	dateISO: "Sy&ouml;t&auml; oike p&auml;iv&auml;m&auml;&auml;r&auml; (VVVV-MM-DD).",
+	number: "Sy&ouml;t&auml; numero.",
+	digits: "Sy&ouml;t&auml; pelk&auml;st&auml;&auml;n numeroita.",
+	equalTo: "Sy&ouml;t&auml; sama arvo uudestaan.",
+	range: jQuery.validator.format("Sy&ouml;t&auml; arvo {0} ja {1} v&auml;lilt&auml;."),
+	max: jQuery.validator.format("Sy&ouml;t&auml; arvo joka on yht&auml; suuri tai suurempi kuin {0}."),
+	min: jQuery.validator.format("Sy&ouml;t&auml; arvo joka on pienempi tai yht&auml; suuri kuin {0}."),
+	creditcard: "Sy&ouml;t&auml; voimassa oleva luottokorttinumero."
+});
+
+      
+    });
+}
+function mce_success_cb(resp){
+    $('#mce-success-response').hide();
+    $('#mce-error-response').hide();
+    if (resp.result=="success"){
+        $('#mce-'+resp.result+'-response').show();
+        $('#mce-'+resp.result+'-response').html(resp.msg);
+        $('#mc-embedded-subscribe-form').each(function(){
+            this.reset();
+    	});
+    } else {
+        var index = -1;
+        var msg;
+        try {
+            var parts = resp.msg.split(' - ',2);
+            if (parts[1]==undefined){
+                msg = resp.msg;
+            } else {
+                i = parseInt(parts[0]);
+                if (i.toString() == parts[0]){
+                    index = parts[0];
+                    msg = parts[1];
+                } else {
+                    index = -1;
+                    msg = resp.msg;
+                }
+            }
+        } catch(e){
+            index = -1;
+            msg = resp.msg;
+        }
+        try{
+            if (index== -1){
+                $('#mce-'+resp.result+'-response').show();
+                $('#mce-'+resp.result+'-response').html(msg);            
+            } else {
+                err_id = 'mce_tmp_error_msg';
+                html = '<div id="'+err_id+'" style="'+err_style+'"> '+msg+'</div>';
+                
+                var input_id = '#mc_embed_signup';
+                var f = $(input_id);
+                if (ftypes[index]=='address'){
+                    input_id = '#mce-'+fnames[index]+'-addr1';
+                    f = $(input_id).parent().parent().get(0);
+                } else if (ftypes[index]=='date'){
+                    input_id = '#mce-'+fnames[index]+'-month';
+                    f = $(input_id).parent().parent().get(0);
+                } else {
+                    input_id = '#mce-'+fnames[index];
+                    f = $().parent(input_id).get(0);
+                }
+                if (f){
+                    $(f).append(html);
+                    $(input_id).focus();
+                } else {
+                    $('#mce-'+resp.result+'-response').show();
+                    $('#mce-'+resp.result+'-response').html(msg);
+                }
+            }
+        } catch(e){
+            $('#mce-'+resp.result+'-response').show();
+            $('#mce-'+resp.result+'-response').html(msg);
+        }
+    }
+}
+
+</script><!--End mc_embed_signup--><br />
+						&nbsp;</td>
+					</tr>
+				</tbody>
+			</table>
+			<a href="itsensa_rakastaminen.html"><img align="center" class="taustaa" src="kuvat/itsensa_rak_illat.jpg" />
+			<a href="avaudu_rakkaudelle.html"><img align="center" class="taustaa" src="kuvat/avaudu_rakkaudelle_tuotteet-360-255.jpg" />
+			<table width="600">
+			<tr>
+			<td width="100">
+			</td>
+			<td width="500"><div class="fb-like" data-href="http://omanelamaniluoja.fi/" data-send="false" data-show-faces="false" data-width="450">&nbsp;</div>
+			</td>
+			</tr>
+			</table>
+			<table align="center" border="0" cellpadding="0" cellspacing="0" width="1000">
+				<tbody>
+					<tr>
+						<td height="215" width="122">&nbsp;</td>
+						<td align="center" height="215" valign="top" width="215"><a href="manifestointi.html"><img class="kurssit" height="215" src="kuvat/manifestointi.jpg" width="215" /></a></td>
+						<td height="210" width="50">&nbsp;</td>
+						<td align="center" height="215" valign="top" width="215"><a href="itsensa_rakastaminen.html"><img class="kurssit" height="215" src="kuvat/itsensarak.jpg" width="215" /></a></td>
+						<td height="215" width="50">&nbsp;</td>
+						<td align="center" height="215" valign="top" width="215"><a href="pelon_kesyttaminen.html"><img class="kurssit" height="215" src="kuvat/pelonkes.jpg" width="215" /></a></td>
+						<td height="215" width="123">&nbsp;</td>
+					</tr>
+					<tr>
+						<td colspan="7" height="40" width="1000">&nbsp;</td>
+					</tr>
+					<tr>
+						<td height="215" width="90">&nbsp;</td>
+						<td align="left" height="215" valign="top" width="215"><a href="ystaviksi_rahan_kanssa.html"><img class="kurssit" height="215" src="kuvat/yrk2.jpg" width="215" /></a></td>
+						<td height="215" width="50">&nbsp;</td>
+						<td align="left" height="215" valign="top" width="215"><a href="tuotteet.html"><img class="kurssit" height="215" src="kuvat/ohjatut_manifharj.jpg" width="215" /></a></td>
+			<td height="150" width="50">&nbsp;</td>
+			<td align="left" height="215" valign="top" width="215"><a href="mentorointi.html"><img class="kurssit" height="215" src="kuvat/manifestointimentorointi.jpg" width="215" /></a></td>
+			<td height="215" width="100">&nbsp;</td>
+		</tr>
+		<tr>
+			<td colspan="7" height="40" width="1000">
+			<div class="fb-like" data-href="http://omanelamaniluoja.fi/" data-send="false" data-show-faces="false" data-width="450">&nbsp;</div>
+			</td>
+		</tr>
+		<tr>
+			<td height="215" width="90"></td>
+			<td align="left" height="215" valign="top" width="215">&nbsp;</td>
+			<td height="215" width="50">&nbsp;</td>
+			<td align="center" height="215" valign="top" width="215">&nbsp;</td>
+			<td height="150" width="50">&nbsp;</td>
+			<td align="center" height="215" valign="top" width="215">&nbsp;</td>
+			<td height="215" width="100">&nbsp;</td>
+		</tr>
+	</tbody>
 </table>
+<br />
+<br />
+&nbsp;
+<p class="valikko2">Copyright &copy; Inna Thil</p>
+</center>
+
+
+
+</body>
+</html>
